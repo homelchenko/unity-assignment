3e971dd05a6ede66604a93f7616b73e9abfe83d4
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-08-14 14:53:46 +0300

    Insert an integer into emtpy linked listed

---
+++ LinkedList/Inserter.Tests/InsertTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using Xunit;
+
+namespace Inserter.Tests
+{
+    public class InsertTests
+    {
+        [Fact]
+        public void Insert_WhenLinkedListIsEmpty_ShouldContainOnlyInsertedItem()
+        {
+            // Arrange
+            var list = new LinkedList<int>();
+
+            // Act
+            list.InsertAscending(5);
+
+            // Assert
+            Assert.Equal(5, list.First.Value);
+        }
+    }
+}
---
+++ LinkedList/Inserter/LinkedListExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Inserter
+{
+    public static class LinkedListExtensions
+    {
+        public static void InsertAscending(this LinkedList<int> list, int value)
+        {
+            list.AddFirst(value);
+        }
+    }
+}
