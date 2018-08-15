ccfbca2782963ef650c63b0d11e5cc6fb4132282
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-08-15 08:19:41 +0300

    Test implicit conversion from integer to fraction

---
+++ Calculator/Fractions.Tests/Fraction_ConversionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Fractions.Tests
+{
+    public class Fraction_ConversionsTests
+    {
+        [Fact]
+        public void ImplicitConverstionFromInteger_ShouldReturnIntegerFraction()
+        {
+            // Act
+            Fraction result = 3;
+
+            // Assert
+            Assert.Equal(new Fraction(3, 1), result);
+        }
+    }
+}
